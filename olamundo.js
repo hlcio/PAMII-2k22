4cff7d8564251be56a0fa3538991cf211f7ab8d1
hlcio <salazar.telecom@gmail.com>
2022-02-21 22:38:37 -0300

    Aula 01

---
+++ olamundo.js
@@ -0,0 +1,15 @@
+console.log('Hello World')
+var a = 6;
+var b = 4;
+let nome = 'John'; 
+console.log('Esse é o conteúdo da variável de a = ',a);
+
+console.log('Esse é o resultado da soma das variáveis ',a,'+',b,'=',a+b);
+
+console.log('Esse é o resultado do produto das variáveis de ',a,'x',b,'=',a*b);
+
+console.log('Esse é o resultado da subtração das variáveis de ',a,'-',b,'=',a-b);
+
+console.log('Esse é o resultado da divisão das variáveis de ',a,'/',b,'=',a/b);
+
+console.log('My name is :',nome);
